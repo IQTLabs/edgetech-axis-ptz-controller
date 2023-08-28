c97b3fbe60f43088d31b78be1d3251e5258ac0f9
Raymond LeClair <raymond.leclair@springbok.io>
2023-08-28 01:02:17 -0400

    Make auto focus option dirty

--- axis-ptz-controller/camera_control.py
+++ axis-ptz-controller/camera_control.py
@@ -54,9 +54,12 @@ class CameraControl(vapix_control.CameraControl):
             Returns the response from the device to the command sent.
 
         """
-        return self._camera_command(
-            {"pan": pan, "tilt": tilt, "zoom": zoom, "speed": speed, "focus": focus}
-        )
+        command = {"pan": pan, "tilt": tilt, "zoom": zoom, "speed": speed}
+        # Defensively only set focus if needed
+        if focus is not None:
+            command["focus"] = focus
+        logging.info(f"command: {command}")
+        return self._camera_command(command)
 
     def get_ptz(self) -> Tuple[float, float, float, float]:
         """
